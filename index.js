0956de8b0b8c94f022b8b08d2254ef23d9b39595
MegaGera <gonzalo.berne@gmail.com>
2025-02-12 22:10:35 +0000

    Added MegaAuth validation

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
+import './config/loadEnv.js';
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
-
-import { PORT, SECRET_JWT_KEY } from './config.js';
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.use(cookieParser());
 
+
+// Validate Api Key
+if (process.env.NODE_ENV === 'production') {
+  const validateApiKey = async (req, res, next) => {
+    try {
+      const headers = new Headers({
+        Cookie: "access_token=" + req.cookies.access_token
+      });
+      const validateRequest = new Request(process.env.VALIDATE_URI, {
+        headers: headers,
+      });
+      const validateResponse = await fetch(validateRequest);
+      const validateData = await validateResponse.json();
+      req.validateData = validateData.data;
+      if (validateResponse.status === 200) {
+        next();
+      } else {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+    } catch (error) {
+      return res.status(401).json({ error: 'Can\'t validate token' });
+    }
+  };
+  app.use(validateApiKey)
+} else if (process.env.NODE_ENV === 'development') {
+  app.use((req, res, next) => {
+    req.validateData = { username: process.env.WEB_USERNAME || 'test' };
+    next();
+  });
+}
+
 app.get('/', (req, res) => {
-  const token = req.cookies.access_token;
-  if (!token) return res.status(401).send('Unauthorized');
-  try {
-    const data = jwt.verify(token, SECRET_JWT_KEY);
-    return res.render('dashboard', data);
-  } catch (error) {
-    return res.status(401).send('Unauthorized');
-  }
+  console.log(req.validateData);
+  return res.render('dashboard', {data: req.validateData});
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
